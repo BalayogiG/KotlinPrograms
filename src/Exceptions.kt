f8eaf565e102208323bd56454f56a287b2de9110
Balayogi G <balayogistark@gmail.com>
2019-01-01 20:39:06 +0530

    First Kotlin application - Checks and Ranges

---
+++ src/Exceptions.kt
@@ -0,0 +1,13 @@
+import java.lang.IllegalArgumentException
+
+fun main(args: Array<String>){
+    val num =90
+    val percentage=
+        if(num in 0..100)
+            num
+        else
+            throw IllegalArgumentException("A percentage value must be "+
+                "between 0 and 100: $num")
+    print(percentage)
+}
+

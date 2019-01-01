52a713056083c0cefc72daee339b6db0699d98e8
Balayogi G <balayogistark@gmail.com>
2019-01-01 19:38:28 +0530

    First Kotlin application - Control structures (if and when)

--- src/Control structures.kt
+++ src/Control structures.kt
@@ -4,10 +4,10 @@ enum class Color{
 fun main(args: Array<String>){
     val a= 10
     val b= 20
-    val c=if(a<b) a else b
+    val c=if(a<b) a else b  //If statement
     println("$c, is smaller")
     val sam="balayogi"
-    val result=when(sam){
+    val result=when(sam){   //when statement
         is String->"is the String of length ${sam.length}"
         else-> "is Integer"
     }

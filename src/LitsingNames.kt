11cb2b443e54f108b4ada5f9cc5f89ac25341be6
Balayogi G <balayogistark@gmail.com>
2019-01-01 11:41:57 +0530

    First Kotlin Application - Listing Strings

---
+++ src/LitsingNames.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>){
+    val readOnlyList = mutableListOf("Java")
+    readOnlyList.add("Kotlin")
+    readOnlyList.add("C")
+    readOnlyList.add("C++")
+    readOnlyList.add("Groovy")
+    readOnlyList.add("ASP")
+    readOnlyList.add(".NET Framework")
+    println(readOnlyList)
+}

966955f8ca75ffc9c3e72040ad623ea24bab0a49
Balayogi G <balayogistark@gmail.com>
2019-01-10 19:48:13 +0530

    First Kotlin application - TypeCast

---
+++ src/TypeCast.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>){
+    upper("to uppercase")
+    lower("TO LOWERCASE")
+}
+
+fun upper(s: String){
+    println(s.toUpperCase())
+}
+
+fun lower(s: String){
+    println(s.toLowerCase())
+}
+
